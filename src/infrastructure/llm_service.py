838fe092195bdd3b7c7e8bd7a6268da683e864e4
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-10-05 12:30:24 +0100

    project updated

--- src/infrastructure/llm_service.py
+++ src/infrastructure/llm_service.py
@@ -1,35 +1,56 @@
 import sys
-from typing import Optional
 
 sys.path.append(".")
-import asyncio
+
+from urllib.parse import urlparse
 from google import genai
-from abc import ABC
-from contextlib import ContextDecorator
-from src.infrastructure.aws_storage import AWSStorageAsync
+from src.infrastructure.aws_storage import AWSStorage
 from src.infrastructure.database import DataBase
-from src.infrastructure.prompt import SYSTEM_PROMPT, get_user_prompt
-from src.infrastructure.redis_service import AsyncRedisBase, RedisLogHandler
-from src.infrastructure.models import Citations
+from src.infrastructure.prompt import (
+    SENTIMENT_SYSTEM_PROMPT,
+    SYSTEM_PROMPT,
+    get_sentiment_user_prompt,
+    get_user_prompt,
+)
+import json
+
+# from src.infrastructure.redis_service import AsyncRedisBase, RedisLogHandler
+from src.infrastructure.models import (
+    Brand_Metrics,
+    Citations,
+    Output_Reports,
+    SentimentBody,
+    Sentiments,
+)
 import markdown2
 from markdownify import markdownify as md
 from src.config import config
 from google.genai.types import GenerateContentConfig
 from datetime import datetime
 import logging
+from selectolax.parser import HTMLParser
+from src.infrastructure.click_house import ClickHouse
+from dateparser import parse
 
 
-class LLMService(ContextDecorator, ABC):
-    def __init__(self, brand_report_id: str, date: str, model: str, brand: str) -> None:
+class LLMService:
+    def __init__(
+        self, brand_report_id: str, date: str, model: str, brand: str, s3_key: str
+    ) -> None:
         # report ids
         self.brand_report_id = brand_report_id
         self.date = date
         self.model = model
         self.brand = brand
+        self.s3_key = s3_key
+        self.text_key = f"{s3_key}/output.txt"
+        self.image_key = f"{s3_key}/screenshot.png"
+
         # initialise others
         self.bucket = config.BUCKET_NAME
         self.client = genai.Client(api_key=config.GEMINI_API_KEY)
-        self.storage = AWSStorageAsync(self.bucket)
+        self.storage = AWSStorage(self.bucket)
+
         # redis
         # # redis_logger = Asyncis_logger)
         # self.redis_handler.setFormatter(
@@ -38,10 +59,14 @@ class LLMService(ContextDecorator, ABC):
         # self.logger.addHandler(self.redis_handler)
         # content
         self.content = ""
+        self.clean_content = ""
         # initialise db
         self.database = DataBase()
+        # initialise clickhouse
+        self.clickhouse = ClickHouse()
 
     def clean_markdown(self, content: str) -> str:
+        print("Cleaning markdown")
         html_content = markdown2.markdown(content)
         cleaned_markdown = md(
             html_content,
@@ -58,72 +83,139 @@ class LLMService(ContextDecorator, ABC):
         )
         return cleaned_markdown.strip()
 
-    # async def save_mentions_to_db(self, results: list[BrandModel]):
-    #     """Save the brand return by LLM into a database"""
-    #     responses: list[BrandDB] = [
-    #         BrandDB(
-    #             prompt_id=self.prompt_id,
-    #             process_id=self.process_id,
-    #             brand_name=result.brand_name,
-    #             mention_count=result.mention_count,
-    #             position=result.position,
-    #             date=datetime.now(),
-    #         )
-    #         for result in results
-    #     ]
-    #     await save_brand_mention(responses)
-    #     self.logger.info(f"- Successfully saved {len(results)} item(s)")
+    def extract_brand_mentions(self):
+        parsed_results = []
+        print("- Starting the LLM prompt parsing system")
+        response = self.client.models.generate_content(
+            model=config.MODEL_NAME,
+            contents=get_user_prompt(self.clean_content),
+            config=GenerateContentConfig(
+                response_mime_type="application/json",
+                response_schema=list[Brand_Metrics],
+                system_instruction=[
+                    SYSTEM_PROMPT,
+                ],
+            ),
+        )
+        # validating and saving to clickhouse
+        results = json.loads(response.model_dump_json())
+        results = response.parsed if response else []
+        if not isinstance(results, list):
+            return None
+        for result in results:
+            parsed_results.append(
+                {
+                    "brand_report_id": result.brand_report_id,
+                    "brand": result.brand,
+                    "mention_count": result.mention_count,
+                    "position": result.position,
+                    "date": parse(self.date),
+                    "model": self.model,
+                }
+            )
+        self.clickhouse.insert_into_db(parsed_results)
 
-    # async def extract_brand_mentions(self, content: str):
-    #     results = []
-    #     self.logger.info(
-    #         "- Starting the LLM prompt parsing system \n - Cleaning Markdown ..."
-    #     )
-    #     clean_content = self.clean_markdown(content)
-    #     self.logger.info(
-    #         "- Markdown Successfully Cleaned \n - Now Generationg Content ..."
-    #     )
-    #     response = await self.client.aio.models.generate_content(
-    #         model=config.MODEL_NAME,
-    #         contents=get_user_prompt(clean_content),
-    #         config=GenerateContentConfig(
-    #             response_mime_type="application/json",
-    #             response_schema=list[BrandModel],
-    #             system_instruction=[
-    #                 SYSTEM_PROMPT,
-    #             ],
-    #         ),
-    #     )
-    #     results = response.parsed if response else []
-    #     self.logger.info("- Successfully Generated Content \n - Now Saving in DB ...")
-    #     return results
+    def save_brand_report_output(self):
+        """Get the brand report outputs"""
+        output_report = Output_Reports(
+            brand_report_id=self.brand_report_id,
+            date=self.date,
+            model=self.model,
+            snapshot=self.image_key,
+            markdown=self.text_key,
+        )
 
-    # async def main(self, s3_key: str) -> Optional[list[BrandModel]]:
-    #     """Download content from s3 and send to LLM for brand extraction"""
-    #     outputs = []
-    #     try:
-    #         await update_llm_process_status(self.process_id, self.prompt_id, "running")
-    #         content = await self.storage.get_file_content(s3_key)
-    #         if not content:
-    #             return None
-    #         clean_content = self.clean_markdown(content)
-    #         outputs = await self.extract_brand_mentions(clean_content)
-    #         if isinstance(outputs, list):
-    #             await self.save_mentions_to_db(outputs)
-    #         await update_llm_process_status(self.process_id, self.prompt_id, "success")
-    #     except Exception as e:
-    #         self.logger.error(f"- Error While Generationg Content : {e}")
-    #         await update_llm_process_status(self.process_id, self.prompt_id, "failed")
-    #     finally:
-    #         self.logger.removeHandler(self.redis_handler)
+        # saving the output report
+        self.database.save_output_reports(output_report)
+
+    def get_sentiments(self):
+        """Get the sentiment with LLM"""
+        print("Getting Sentiments with LLM")
+        parsed_sentiments = []
+        response = self.client.models.generate_content(
+            model=config.MODEL_NAME,
+            contents=get_sentiment_user_prompt(self.clean_content),
+            config=GenerateContentConfig(
+                response_mime_type="application/json",
+                response_schema=list[SentimentBody],
+                system_instruction=[
+                    SENTIMENT_SYSTEM_PROMPT,
+                ],
+            ),
+        )
+
+        # Validate sentiments
+        sentiments = response.parsed if response.parsed else []
+        if not isinstance(sentiments, list):
+            return None
+        for sentiment in sentiments:
+            parsed_sentiments.append(
+                Sentiments(
+                    brand_report_id=self.brand_report_id,
+                    date=self.date,
+                    model=self.model,
+                    brand=sentiment.brand,
+                    brand_model=sentiment.brand_model,
+                    positive_phrases=sentiment.positive_phrases,
+                    negative_phrases=sentiment.negative_phrases,
+                )
+            )
+
+        # save the sentiments
+        self.database.save_sentiments(parsed_sentiments)
+        return None
+
+    def get_citations(self):
+        """Convert markdown to html and extract links"""
+        print("Getting the citations")
+        citations = []
+        html_content = markdown2.markdown(self.clean_content)
+        html = HTMLParser(html_content)
+        link_nodes = html.css("a")
+        for i, link_node in enumerate(link_nodes):
+            link = link_node.css_first("a")
+            if not link:
+                continue
+            title = link.text(separator=" ")
+            link = link.attributes.get("href")
+            domain = urlparse(link).netloc if link else None
+            parsed = urlparse(link)
+            clean_url = clean_url = f"{parsed.scheme}://{parsed.netloc}{parsed.path}"
+            citation = Citations(
+                brand_report_id=self.brand_report_id,
+                date=self.date,
+                model=self.model,
+                brand=self.brand,
+                rank=i + 1,
+                title=title,
+                domain=domain,
+                norm_url=clean_url,
+            )
+            citations.append(citation)
+        print(f"Found -> {len(citations)} citations")
+        # once citations have been validated then save
+        self.database.save_citations(citations)
+
+    def main(self) -> None:
+        """Start the whole parser workflow"""
+        # download the content from s3
+        content = self.storage.get_file_content(self.text_key)
+        if not content:
+            return None
+
+        # clean the content
+        self.clean_content = self.clean_markdown(content)
+
+        # get and save the citations
+        # self.get_citations()
+        # self.get_sentiments()
+        # self.save_brand_report_output()
+        self.extract_brand_mentions()
 
 
 if __name__ == "__main__":
-
-    async def main():
-        s3_key = "chatgpt/1756805263/output.txt"
-        llm_service = LLMService("prompt_12345", "process_12345")
-        output = await llm_service.main(s3_key)
-        print(output)
-
-    asyncio.run(main())
+    llm_service = LLMService(
+        "br_12345", "2023-01-01", "Chatgpt", "Nike", "chatgpt/1758754781"
+    )
+    llm_service.database.create_all_tables()
+    llm_service.main()
--- src/infrastructure/models.py
+++ src/infrastructure/models.py
@@ -1,19 +1,28 @@
 from typing import Optional
+from pydantic import BaseModel
 from sqlmodel import JSON, Field, SQLModel
 
 
+class SentimentBody(BaseModel):
+    brand: str
+    brand_model: str
+    positive_phrases: list[str] = Field(sa_type=JSON)
+    negative_phrases: list[str] = Field(sa_type=JSON)
+
+
 class Sentiments(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+    id: int = Field(primary_key=True, default=None)
     brand_report_id: str
     date: str
     model: str
     brand: str
+    brand_model: Optional[str]
     positive_phrases: list[str] = Field(sa_type=JSON)
-    negative_phrase: list[str] = Field(sa_type=JSON)
+    negative_phrases: list[str] = Field(sa_type=JSON)
 
 
-class Citation(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+class Citations(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, default=None)
     brand_report_id: str
     date: str
     model: str
@@ -22,3 +31,21 @@ class Citation(SQLModel, table=True):
     title: Optional[str]
     domain: Optional[str]
     norm_url: Optional[str]
+
+
+class Output_Reports(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, default=None)
+    brand_report_id: str
+    date: str
+    model: str
+    snapshot: str
+    markdown: str
+
+
+class Brand_Metrics(BaseModel):
+    brand_report_id: str
+    date: str
+    model: str
+    brand: str
+    mention_count: int
+    position: int
