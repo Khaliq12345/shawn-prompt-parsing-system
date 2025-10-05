80a1b13c5aa17cd0fc71fee54e2037f3a15d213f
gbohouili <eldomoreogbohouili@gmail.com>
2025-10-05 20:28:10 +0100

    Add citations and get_report_output endpoints with DB integration

--- src/api/app.py
+++ src/api/app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, HTTPException, Security, Depends
 from fastapi.security.api_key import APIKeyHeader
 from fastapi.middleware.cors import CORSMiddleware
 from src.api.v1.metrics import router as metrics_router
+from src.api.v1.prompts import router as prompts_router
 from src.config.config import APP_PORT, ENV, API_KEY
 from src.infrastructure.database import DataBase
 
@@ -42,6 +43,8 @@ app.add_middleware(
 # app.include_router(prefix="/api", router=logs_router, tags=["LOGS"])
 app.include_router(prefix="/api", router=metrics_router, tags=["METRICS"])
 
+# Ajouter la route pour prompts
+app.include_router(prefix="/api", router=prompts_router, tags=["PROMPTS"])
 
 def start_app():
     uvicorn.run(
